824cf85256f4f23848873cf885e663a109fdf3be
Nischayn22 <nischayn22@gmail.com>
2018-07-09 23:46:54 +0530

    lots of new stuff

--- gravityformsImporter.php
+++ gravityformsImporter.php
@@ -4,11 +4,14 @@
  * @license GPL v2 or later
  */
 
-error_reporting( E_STRICT );
-date_default_timezone_set('Asia/Kolkata');
+error_reporting( E_ALL );
+
+# Includes the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
 
 include( 'settings.php' );
-include 'MediaWiki_Api/MediaWiki_Api_functions.php';
+$settings['cookiefile'] = "cookies.tmp";
+use Nischayn22\MediaWikiApi;
 
 function calculate_signature( $string, $private_key ) {
     $hash = hash_hmac( 'sha1', $string, $private_key, true );
@@ -29,28 +32,63 @@ $string_to_sign = sprintf( '%s:%s:%s:%s', $api_key, $method, $route, $expires );
 $sig = calculate_signature( $string_to_sign, $private_key );
 
 $url = $base_url . $route . '?api_key=' . $api_key . '&signature=' . $sig . '&expires=' . $expires;
-$response = json_decode( httpRequest( $url ) );
+$response = json_decode( MediaWikiApi::httpRequest( $url ) );
 $form_fields = array();
 foreach($response->response->fields as $field) {
 	$form_fields[$field->id] = $field->label;
+	if ( is_array( $field->inputs ) ) {
+		foreach( $field->inputs as $input ) {
+			$form_fields[$input->id] = $input->label;
+		}
+	}
 }
-
-
 $route = "forms/$form_id/entries";
 $string_to_sign = sprintf( '%s:%s:%s:%s', $api_key, $method, $route, $expires );
 $sig = calculate_signature( $string_to_sign, $private_key );
- 
+
+$wikiApi = new MediaWikiApi($settings['wiki_api']);
+echo "Logging in to the wiki\n";
+$wikiApi->logout();
+if( $wikiApi->login($settings['wiki_user'], $settings['wiki_pass']) ) {
+	echo "Successfully logged in\n";
+} else {
+	echo "Login failed!\n";
+}
+
 $url = $base_url . $route . '?api_key=' . $api_key . '&signature=' . $sig . '&expires=' . $expires;
-$response = json_decode( httpRequest( $url ) );
+$response = json_decode( MediaWikiApi::httpRequest( $url ) );
+$name_fields = explode( ' ', $settings['page_name'] );
 foreach($response->response->entries as $entry) {
+	$entry = (array) $entry;
 	$field_values = array();
 	foreach( $entry as $key => $value ) {
 		if (array_key_exists($key, $form_fields)) {
-			$field_values[$form_fields[$key]] = $value;
+			if ( $settings['upload_links'] && strstr($value, 'http') ) {
+				file_put_contents( basename( $value ), file_get_contents( $value ) );
+				if ( $wikiApi->upload( basename( $value ), __DIR__ . '/' . basename( $value ) ) ) {
+					$value = basename( $value );
+				}
+				unlink( __DIR__ . '/' . basename( $value ) );
+			}
+			$field_values[$key] = $value;
 		}
 	}
-	file_put_contents("test.data", json_encode($field_values));
-	var_dump($field_values);
-	die();
-	
+	$pageName = array();
+	foreach	( $name_fields as $field ) {
+		$pageName[] = $entry[$field];
+	}
+	$pageName = implode( ' ', $pageName );
+	$content = "
+	{{" . $settings['template_name'] . "
+	|" . 
+	implode("|\n", array_map(
+		function ($v, $k) { return $k . '=' . $v; },
+		$field_values,
+		array_keys( $field_values )
+		)
+	) . 
+	"}}";
+	$wikiApi->editPage( $pageName, $content );
+	echo "Copied page $pageName \n";
 }
+echo "All done. \n";
--- settings.php
+++ settings.php
@@ -5,3 +5,11 @@ $settings['api_key'] = 'abc';
 $settings['private_key'] = 'xyz';
 
 $settings['form_id'] = '2';
+
+$settings['wiki_api'] = "http://www.mywiki.org/"; // Location of Api.php
+$settings['wiki_user'] = "Nischay"; // Username of account with read permissions
+$settings['wiki_pass'] = "password"; // Password
+
+$settings['template_name'] = 'FormEntry';
+$settings['upload_links'] = true;
+$settings['page_name'] = "date_created"; // Form fields separated by space
